48df2f80c14290a03a2d527c14b36314003e00eb
NandikaSahu0711 <79842952+NandikaSahu0711@users.noreply.github.com>
2021-09-02 19:58:07 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+heading_console = "My First Console Project";
+console.log(heading_console);
+
+first_name = "Ranbir";
+last_name = "Kapoor";
+
+var full_name = first_name.concat(last_name);
+console.log(full_name);
+
+var a = 10;
+var b = 5;
+
+var add = a + b ;
+console.log(add);
+
+var sub = a - b ;
+console.log(sub);
+
+var multiple = a * b ;
+console.log(multiple);
+
+var divide = a / b ;
+console.log(divide);
+    
